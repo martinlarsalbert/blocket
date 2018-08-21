9bc216028b095b79eac6abb6face6e23108d07e2
martinlarsalbert <marale@kth.se>
2018-08-21 19:15:42 +0200

    now then...

--- update_blocket.py
+++ update_blocket.py
@@ -2,8 +2,7 @@
 Module that inhabits some methods to download cars from blocket.se
 
 """
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/python3.6
 
 from requests import get
 from requests.exceptions import RequestException
